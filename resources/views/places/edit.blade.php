92fed4792fe525a55e07ef5bff535c948b862162
AndySong <andylauszp@gmail.com>
2019-02-03 01:16:57 +1300

    wip

--- resources/views/places/edit.blade.php
+++ resources/views/places/edit.blade.php
@@ -1,40 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="columns is-centered">
-            <div class="column is-half">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            New Place
-                        </p>
-                    </header>
-                    <div class="card-content">
-                        <form method="post" action="{{url('/places/'.$place->id)}}" class="box">
-                            @csrf
-                            @method('PATCH')
-                            <div class="field is-grouped">
-                                <div class="control is-expanded {{$errors->has('name')?'has-icons-right':''}}">
-                                    <input class="input {{$errors->has('name')?'is-danger':''}}" type="text"
-                                           name="name"
-                                           aria-label="edit"
-                                           value="{{old('name',$place->name)}}">
-                                    @if($errors->has('name'))
-                                        <span class="icon is-small is-right has-text-danger">
-                                            <i class="fas fa-exclamation-triangle"></i>
-                                        </span>
-                                        <p class="help is-danger">{{$errors->first('name')}}</p>
-                                    @endif
-                                </div>
-                                <div class="control">
-                                    <button class="button is-info" type="submit">Edit Place</button>
-                                </div>
+<div class="container">
+    <div class="columns is-centered">
+        <div class="column is-half">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Edit Place
+                    </p>
+                </header>
+                <div class="card-content">
+                    <form method="post" action="{{url('/places/'.$place->id)}}" class="box">
+                        @csrf
+                        @method('PATCH')
+                        <div class="field is-grouped">
+                            <div class="control is-expanded {{$errors->has('name')?'has-icons-right':''}}">
+                                <input class="input {{$errors->has('name')?'is-danger':''}}" type="text" name="name"
+                                    aria-label="edit" value="{{old('name',$place->name)}}">
+                                @if($errors->has('name'))
+                                <span class="icon is-small is-right has-text-danger">
+                                    <i class="fas fa-exclamation-triangle"></i>
+                                </span>
+                                <p class="help is-danger">{{$errors->first('name')}}</p>
+                                @endif
                             </div>
-                        </form>
-                    </div>
+                            <div class="control">
+                                <button class="button is-info" type="submit">Save</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="columns is-centered">
-            <div class="column is-half">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            All Places
-                        </p>
-                    </header>
-                    <div class="card-content">
-                        <nav class="panel">
-                            @foreach($places as $place)
-                                <a class="panel-block" href="{{url('/places/'.$place->id.'/edit')}}">
-                                    <span class="panel-icon">
-                                        <i class="fas fa-map-pin" aria-hidden="true"></i>
-                                    </span>{{$place->name}}</a>
-                            @endforeach
-                            <div class="panel-block">
-                                <a href="{{url('/places/create')}}" class="button is-link is-outlined is-fullwidth">
-                                    Add new place
-                                </a>
-                            </div>
-                        </nav>
-                    </div>
+<div class="container">
+    <div class="columns is-centered">
+        <div class="column is-half">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        All Places
+                    </p>
+                </header>
+                <div class="card-content">
+                    <nav class="panel">
+                        @foreach($places as $place)
+                        <a class="panel-block" href="{{url('/places/'.$place->id.'/edit')}}">
+                            <span class="panel-icon">
+                                <i class="fas fa-map-pin" aria-hidden="true"></i>
+                            </span>{{$place->name}}</a>
+                        @endforeach
+                        <div class="panel-block">
+                            <a href="{{url('/places/create')}}" class="button is-link is-outlined is-fullwidth">
+                                Add new place
+                            </a>
+                        </div>
+                    </nav>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
