cb4b4d99cd4e1624a1d2d32ce0a38488ed9009ce
AndySong <andylauszp@gmail.com>
2019-02-03 00:08:40 +1300

    add v-cloak

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,12 +1,12 @@
-<navigation-bar inline-template>
+<navigation-bar inline-template v-cloak>
     <div class="container">
         <div class="navbar-brand">
             <a class="navbar-item" href="{{ url('/') }}">
                 <img src="{{ asset('logo/checklist.svg') }}" alt="Shopping list" width="24" height="24">
                 {{ config('app.name', 'Laravel') }}
             </a>
-            <a @click="toggle" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
-               :class="{ 'is-active': show }" data-target="navbarBasicExample">
+            <a @click="toggle" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" :class="{ 'is-active': show }"
+                data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
@@ -23,29 +23,29 @@
             </div>
             <div class="navbar-end">
                 @guest
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-danger" href="{{ route('register') }}">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-danger" href="{{ route('register') }}">
                                 <strong>Sign up</strong>
                             </a>
-                            <a class="button is-light" href="{{ route('login') }}">Log in</a>
-                        </div>
+                        <a class="button is-light" href="{{ route('login') }}">Log in</a>
                     </div>
+                </div>
                 @else
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
                             {{ Auth::user()->name }}
                         </a>
-                        <div class="navbar-dropdown">
-                            <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">
                                 Log out
                             </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
+                </div>
                 @endguest
             </div>
         </div>
