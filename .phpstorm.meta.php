e0aff15d23050090919f77bb03e2b81c0966ed86
AndySong <andylauszp@gmail.com>
2019-02-06 11:59:08 +1300

    add PlaceTest

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-02-04 21:24:55.
+    * Generated on 2019-02-05 21:32:38.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
---
+++ tests/Feature/PlacesTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PlacesTest extends TestCase
+{
+    use RefreshDatabase,WithFaker;
+
+    public function test_a_user_can_create_a_place()
+    {
+        $attributes = [
+            'name' => 'andy song',
+            'slug' => 'andy-song',
+        ];
+
+        $this->post('/places', $attributes)->assertRedirect('/places');
+
+        $this->assertDatabaseHas('places', $attributes);
+
+        $this->get('/places')->assertSee($attributes['name']);
+
+        $this->get('/places')->assertSee($attributes['slug']);
+    }
+
+    public function test_a_place_requires_a_name_min_3()
+    {
+        $array = [];
+        for ($i = 0; $i < 3; ++$i) {
+            $place = factory('App\Place')->raw(['name' => $i]);
+            $array[$i] = $place;
+        }
+
+        foreach ($array as $attribute) {
+            $this->post('/places', $attribute)->assertSessionHasErrors('name');
+        }
+    }
+}
