c2256cb960bcb319b7c189d80798881949029cb9
AndySong <andylauszp@gmail.com>
2019-02-09 12:32:10 +1300

    remember current select value

--- resources/views/items/form.blade.php
+++ resources/views/items/form.blade.php
@@ -5,18 +5,18 @@
             <div class="select">
                 <select aria-label="select" name="place_id">
                     @foreach($places as $place)
-                    <option value="{{$place->id}}">{{$place->name}}</option>
+                        <option value="{{$place->id}}" {{old('place_id')==$place->id?'selected':''}}>{{$place->name}}</option>
                     @endforeach
                 </select>
             </div>
         </div>
         <div class="control is-expanded {{$errors->has('body')?'has-icons-right':''}}">
             <input class="input {{$errors->has('body')?'is-danger':''}}" type="text" name="body" value="{{old('body')}}"
-                placeholder="add"> @if($errors->has('body'))
-            <span class="icon is-small is-right has-text-danger">
+                   placeholder="add"> @if($errors->has('body'))
+                <span class="icon is-small is-right has-text-danger">
                 <i class="fas fa-exclamation-triangle"></i>
             </span>
-            <p class="help is-danger">{{$errors->first('body')}}</p>
+                <p class="help is-danger">{{$errors->first('body')}}</p>
             @endif
         </div>
         <div class="control">
